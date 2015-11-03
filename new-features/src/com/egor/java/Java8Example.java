093cc2eee5cf3e06bb3d916222c6db0dae4c4f19
k3naks <shibeko.egor@gmail.com>
2015-11-03 16:57:12 +0300

    in test branch

--- new-features/src/com/egor/java/Java8Example.java
+++ new-features/src/com/egor/java/Java8Example.java
@@ -12,6 +12,7 @@ public class Java8Example {
     public static  void main(String[] args) {
         System.out.println("Test");
         System.out.println("this test for git");
+        System.out.println("agahahdh");
         System.out.println(calculate((double a, double b) ->
                 a + b
                 , 2d, 5d));
